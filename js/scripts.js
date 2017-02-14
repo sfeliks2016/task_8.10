c24295a7a94bad48027dfa072729144a61e81115
Szymon Feliks <sz.feliks@gmail.com>
2017-02-14 14:56:12 +0100

    change scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,23 +3,24 @@
 
 var withButtonClass = document.getElementsByClassName('button');
 
-console.log(withButtonClass);
+console.log(withButtonClass); // wtswietli tablice elementow z class='button'
 
-console.log(withButtonClass.length);
-
-
-// LUB zgodnie z HTML DOM length Property
-/*
-var nodelist = document.getElementsByClassName('button').length;
-
-console.log(nodelist);
-
-*/
+console.log(withButtonClass.length);// wyswietli dlugosc tablicy
 
 
-//pętla, która wykona się tyle razy, ile jest elementów z class='button' na stronie - uzycie wlasciwosci: length.
 
-//pętli wykonuje funkcję, która wyświetla tekst zawarty w każdym z tych buttonów.
+//pętla wykonuje funkcję, która wyświetla tekst zawarty w każdym z tych buttonów za pomoca funkcji alert().
+
+for (var i=0; i< withButtonClass.length;i++){
+  
+  var tab= document.getElementsByClassName("button")[i].textContent; // textContent zamiast innerText!
+    
+    alert(tab);     
+}
+
+//pętla wykonuje się tyle razy, ile jest elementów z class='button' na stronie - uzycie wlasciwosci: length.
+
+//pętli wykonuje funkcję, która wyświetla w konsoli tekst zawarty w każdym z tych buttonów.
 
 for (var i=0; i< withButtonClass.length;i++){
   
